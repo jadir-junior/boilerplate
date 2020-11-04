f8a8c08b50f4872e8429e1d02274de98a218697d
Jadir Junior <jadirjsjunior@gmail.com>
2020-11-04 20:02:17 -0300

    config another things

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,21 @@
-const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+import * as S from './styles'
+
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJs, NextJS e Styled Components'
+}) => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átamo e React Avançado escrito ao lado."
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para um tela com código."
+    />
+  </S.Wrapper>
 )
 
 export default Main
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -9,12 +9,12 @@ describe('<Main />', () => {
     expect(
       screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
-    expect(container.firstChild).toMatchInlineSnapshot(`
-      <main>
-        <h1>
-          React Avançado
-        </h1>
-      </main>
-    `)
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
